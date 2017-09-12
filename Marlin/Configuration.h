33b006f1c9187a39cd80d028ec088c382b0ff588
Daniel Raniz Raneland <raniz@raneland.se>
2017-09-12 17:55:51 +0200

    Changed ABS preheat to PET-G

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1051,8 +1051,8 @@
 #define PREHEAT_1_TEMP_BED     70
 #define PREHEAT_1_FAN_SPEED     0 // Value from 0 to 255
 
-#define PREHEAT_2_TEMP_HOTEND 240
-#define PREHEAT_2_TEMP_BED    100
+#define PREHEAT_2_TEMP_HOTEND 245
+#define PREHEAT_2_TEMP_BED    80
 #define PREHEAT_2_FAN_SPEED     0 // Value from 0 to 255
 
 /**
